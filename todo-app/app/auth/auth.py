f34c4b3f0b0bd014f52da8f85f4063f60e97cbaa
BlainePrice <bpersonal01@gmail.com>
2025-07-08 12:25:15 -0500

    Delete todo-app directory
    testing concluded a submodule was needed

--- todo-app/app/auth/auth.py
+++
@@ -1,7 +0,0 @@
-from passlib.hash import bcrypt
-
-def hash_password(password: str):
-    return bcrypt.hash(password)
-
-def verify_password(plain_password, hashed_password):
-    return bcrypt.verify(plain_password, hashed_password)
